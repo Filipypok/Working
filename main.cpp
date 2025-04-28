478a558d43104a02346f9c86c8d8c5b114c87fd9
Maks Filipypok <generalov.maks.84@gmail.com>
2025-04-28 23:09:15 +0300

    Вывод реализации паттернов

--- main.cpp
+++ main.cpp
@@ -90,7 +90,7 @@ private:
     }
     void DetectedhavefiveG()
     {
-        if(havefiveG())
+        if(haveFiveG())
         {
             cout<<"very good!";
         }
@@ -113,7 +113,7 @@ public:
 
     Smartfone(mobileColor color) : battery(3470), smartcolor(color), screen(9), /*children*/ targetManner(nullptr)
     {
-        fiveG = static_cast<bool>(rand()%2);
+        FiveG = static_cast<bool>(rand()%2);
     }
     ~Smartfone()
      {
@@ -123,11 +123,11 @@ public:
 public:
     int screen;
 
-    bool havefiveG() const {return fiveG;}
+    bool haveFiveG() const {return FiveG;}
 
     virtual void checking()
     {
-        if(havefiveG())
+        if(haveFiveG())
         {
             cout<<"It's very cool and modern smartfone";
         }
@@ -139,7 +139,7 @@ public:
     void target()
     {
         printType();
-        cout<<":"
+        cout<<":";
         DetectedhavefiveG();
         cout<< ":";
 
@@ -156,11 +156,265 @@ public:
 
 
 
+class chipmobile : public Smartfone
+{
+protected:
+    int think;
+public:
+    chipmobile() : Smartfone(mobileColor:: Blue) {SetTargerManner(CreateTargetStrategy(TargetEnum::Gamers));}
+    ~chipmobile() {}
 
+    void printType()  {cout<<"Chip mobile";}
+    void Case() {cout<<"Plastic case";}
+    void screen()  {cout<<"screen";}
+};
+
+class simplemobile : public Smartfone
+{
+protected:
+    bool medage;
+public:
+    simplemobile() : Smartfone(mobileColor:: White) {SetTargerManner(CreateTargetStrategy(TargetEnum:: Photographers));}
+
+    ~simplemobile() {}
+
+    void printType() {cout<<"Simple mobile";}
+    void Case() {cout<<"Metal case";}
+    void screen() {cout<<"screen";}
+
+};
+
+class expensiveMobile : public Smartfone
+{
+public:
+    expensiveMobile() : Smartfone(mobileColor::Black) {SetTargerManner(CreateTargetStrategy(TargetEnum::Businessman));}
+    ~expensiveMobile() {}
+    void printType() {cout<< "Expensive mobile";}
+    void Case() {cout<<"Premium case";}
+    void screen() {cout<<"screen";}
+};
+
+
+
+
+enum class SmartfoneType : int
+{
+    expensiveMobile = 1,
+    simplemobile = 2,
+    chipmobile = 3,
+
+    Undefined = 0
+};
+
+Smartfone* CreateSmartfone(SmartfoneType i)
+{
+    Smartfone* newSmartfone = nullptr;
+
+    if(i==SmartfoneType::chipmobile)
+    {
+        newSmartfone = new chipmobile;
+    }
+
+    else if (i==SmartfoneType::simplemobile)
+    {
+        newSmartfone = new simplemobile;
+    }
+    else if (i == SmartfoneType::expensiveMobile)
+    {
+        newSmartfone = new expensiveMobile;
+    }
+    return newSmartfone;
+}
+
+void AllGetMoney(Iterator<Smartfone*>* it)
+{
+    for (it ->First(); !it->IsDone(); it->Next())
+    {
+        Smartfone* currentSmartfone = it->GetCurrent();
+        currentSmartfone->checking();
+    }
+}
+
+void TargetEmAll(Iterator<Smartfone*> *it)
+{
+    for(it->First(); !it->IsDone(); it->Next())
+    {
+        Smartfone *currentSmartfone = it->GetCurrent();
+        currentSmartfone -> target();
+    }
+}
+
+void TargetEmAllGood(Iterator<Smartfone*> *it)
+{
+    for(it->First(); !it->IsDone(); it->Next())
+    {
+        Smartfone *currentSmartfone = it->GetCurrent();
+        if(!currentSmartfone->haveFiveG()) continue;
+
+        currentSmartfone->target();
+    }
+
+}
+
+void TargetEmAllBlue(Iterator<Smartfone*> *it)
+{
+    for(it->First(); !it->IsDone(); it->Next())
+    {
+        Smartfone *currentSmartfone = it->GetCurrent();
+        if(currentSmartfone->GetColor() != mobileColor::White) continue;
+
+
+        currentSmartfone->target();
+    }
+
+}
+
+
+class SmartfoneColorDecorator : public IteratorDecorator<class Smartfone*>
+{
+private:
+    mobileColor TargetColor;
+public:
+    SmartfoneColorDecorator(Iterator<Smartfone*>* it, mobileColor color)
+        : IteratorDecorator<Smartfone*>(it), TargetColor(color) {}
+
+    void First()
+{
+    this->It->First();
+    while (!this->It->IsDone() && It->GetCurrent()->GetColor() != TargetColor)
+    {
+        this->It->Next();
+    }
+}
+
+    void Next()
+{
+    do
+    {
+        this->It->Next();
+
+    } while (!this->It->IsDone() && It->GetCurrent()->GetColor() != TargetColor);
+}
+};
+
+class SmartfoneFiveGDecorator : public IteratorDecorator<class Smartfone*>
+{
+private:
+    bool TargetGood;
+public:
+    SmartfoneFiveGDecorator(Iterator<Smartfone*>* it, bool FiveG) : IteratorDecorator<Smartfone*>(it), TargetGood(FiveG) {}
+
+    void First()
+{
+    this->It->First();
+    while (!this->It->IsDone() && this->It->GetCurrent()->haveFiveG() != TargetGood)
+    {
+        this->It->Next();
+    }
+
+}
+    void Next()
+{
+    do
+    {
+        this->It->Next();
+
+    } while (!this->It->IsDone() && this->It->GetCurrent()->haveFiveG() != TargetGood);
+}
+};
 
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    srand(time(nullptr)); // Инициализация генератора
+    int N = 5;
+
+    StackClass<Smartfone*> SmartfoneStack;
+
+    for(size_t i = 0; i < N; i++)
+    {
+        int Smartfone_num = rand() %3+1;
+        SmartfoneType smartfone_type = static_cast<SmartfoneType>(Smartfone_num);
+        Smartfone* newSmartfone = CreateSmartfone(smartfone_type);
+        SmartfoneStack.Push(newSmartfone);
+    }
+
+    cout<< "size steck smartfone: "<< SmartfoneStack.Size() << endl;
+
+    Iterator<Smartfone*>* it2 = SmartfoneStack.GetIterator();
+
+    cout<<endl;
+
+    Array<Smartfone*> SmartfoneArray;
+    for (size_t i = 0; i < N; i++)
+    {
+        int Smartfone_num = rand() %3+1;
+        SmartfoneType smartfone_type = static_cast<SmartfoneType>(Smartfone_num);
+        Smartfone* newSmartfone = CreateSmartfone(smartfone_type);
+        SmartfoneArray.Add(newSmartfone);
+    }
+
+        wcout << "size array smartfone: " << SmartfoneStack.Size() << endl;
+
+
+    cout << "" << endl;
+    list<Smartfone*> SmartfoneVector;
+    for (size_t i = 0; i < N; i++)
+    {
+        int Smartfone_num = rand() % 3 + 1;
+        SmartfoneType smartfone_type = static_cast<SmartfoneType>(Smartfone_num);
+        Smartfone* newSmartfone = CreateSmartfone(smartfone_type);
+        SmartfoneVector.push_back(newSmartfone);
+    }
+
+    cout << "Stack class";
+
+        cout << endl << "size stack: " << SmartfoneStack.Size() << endl;
+        Iterator<Smartfone*> *it7 = SmartfoneStack.GetIterator();
+        TargetEmAll(it7);
+    cout << endl << "Black Smartfone have 5G:" << endl;
+        Iterator<Smartfone*>* FiveGSkinIt = new SmartfoneFiveGDecorator(new SmartfoneColorDecorator(SmartfoneStack.GetIterator(),mobileColor::Black), true);
+        TargetEmAll(FiveGSkinIt);
+        delete FiveGSkinIt;
+
+    cout << "Array class" << endl;
+    cout << endl << "Smartfone have 5G:" << endl;
+    Iterator<Smartfone*>* FiveGIt = new SmartfoneFiveGDecorator(SmartfoneArray.GetIterator(), true);
+    TargetEmAll(FiveGIt);
+    delete FiveGIt;
+
+    cout << endl << "black smartfones:" << endl;
+    Iterator<Smartfone*>* SkinIt = new SmartfoneColorDecorator(SmartfoneArray.GetIterator(), mobileColor::Black);
+    TargetEmAll(SkinIt);
+    delete SkinIt;
+
+    cout << endl << "black smartfone have 5G:" << endl;
+    Iterator<Smartfone*>* haveFiveGIt =
+        new SmartfoneFiveGDecorator(new SmartfoneColorDecorator(SmartfoneArray.GetIterator(), mobileColor::Black), true);
+    TargetEmAll(haveFiveGIt);
+    delete haveFiveGIt;
+
+    cout << endl << "black smartfone  skin have 5G using adapted iterator (another container):" << endl;
+    Iterator<Smartfone*>* adaptedIt = new ConstIteratorAdapter<std::list<Smartfone*>, Smartfone*>(&SmartfoneVector);
+    Iterator<Smartfone*>* adaptedBlackIt = new SmartfoneFiveGDecorator(new SmartfoneColorDecorator(adaptedIt, mobileColor::Black), true);
+    TargetEmAll(adaptedBlackIt);
+    delete adaptedBlackIt;
+
+    cout << "STL:" << endl;
+    vector<Smartfone*> stlVector;
+        for (int i = 0; i < 5; ++i) {
+            int Smartfone_num = rand() % 3 + 1;
+            SmartfoneType smartfone_type = static_cast<SmartfoneType>(Smartfone_num);
+            Smartfone* newSmartfone = CreateSmartfone(smartfone_type);
+            stlVector.push_back(newSmartfone);
+    }
+
+        Iterator<Smartfone*>* stlIt = new ConstIteratorAdapter<vector<Smartfone*>, Smartfone*>(&stlVector);
+        TargetEmAll(stlIt);
+        delete stlIt;
     return 0;
 }
+
+
+
+
