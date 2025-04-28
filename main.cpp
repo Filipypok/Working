f5a459f327c9a0cf5235b5b8f0e0b74e0c1c8586
Maks Filipypok <generalov.maks.84@gmail.com>
2025-04-28 20:44:12 +0300

    Создание классов для стратегий

--- main.cpp
+++ main.cpp
@@ -1,6 +1,47 @@
-#include <iostream>
+#include<iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include "patternh.h"
+#include <vector>
+#include <list>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
+
+
+enum class mobileColor: int
+{
+    Black,
+    Yellow,
+    White,
+    Green
+};
+
+enum class TargetEnum: int
+{
+    gamers,
+    photographers,
+    businessman,
+
+    aesthetics
+
+    None
+};
+
+class TargetStrategy{
+public:
+    virtual ~TargetStrategy(){}
+    virtual void target() =0;
+};
+
+
+
+
+
+
+
+
 
 int main()
 {
